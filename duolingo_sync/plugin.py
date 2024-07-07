dd0845318e385bcdb267cfdd3a4c7566e846f186
Joseph Schilz <joseph@schilz.org>
2024-07-07 09:42:52 -0700

    Incorporate new fixes from the duolingo library.

--- duolingo_sync/plugin.py
+++ duolingo_sync/plugin.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Optional, List
+import traceback
 
 import requests.exceptions
 from anki.utils import splitFields, ids2str
@@ -12,7 +13,7 @@ from aqt.operations import QueryOp
 from aqt.qt import *
 from aqt.utils import askUser, showWarning
 from aqt.utils import showInfo
-from .duolingo import Duolingo, LoginFailedException
+from .duolingo import Duolingo
 from .duolingo_display_login_dialog import duolingo_display_login_dialog
 from .duolingo_model import get_duolingo_model
 
@@ -34,7 +35,7 @@ class AddVocabResult:
     problem_vocabs: List[str] = field(default_factory=list)
 
 
-def login_and_retrieve_vocab(username, password) -> VocabRetrieveResult:
+def login_and_retrieve_vocab(jwt) -> VocabRetrieveResult:
     result = VocabRetrieveResult(success=False, words_to_add=[])
 
     model = get_duolingo_model(aqt)
@@ -50,39 +51,28 @@ def login_and_retrieve_vocab(username, password) -> VocabRetrieveResult:
             )
         )
 
-        lingo = Duolingo(username, password)
+        lingo = Duolingo(jwt=jwt)
 
         aqt.mw.taskman.run_on_main(
             lambda: aqt.mw.progress.update(
                 label=f"Retrieving vocabulary...",
             )
         )
-        vocabulary_response = lingo.get_vocabulary()
 
-    except LoginFailedException:
-        aqt.mw.taskman.run_on_main(
-            lambda: showWarning(
-                """
-                <p>Logging in to Duolingo failed. Please check your Duolingo credentials.</p>
-
-                <p>Having trouble logging in? You must use your <i>Duolingo</i> username and JWT.
-                You <i>can't</i> use your Google or Facebook credentials, even if that's what you use to
-                sign in to Duolingo.</p>
-
-                <p>You can find your Duolingo username at
-                <a href="https://www.duolingo.com/settings">https://www.duolingo.com/settings</a>.</p>
-                """
-            )
-        )
-        return result
     except requests.exceptions.ConnectionError:
         aqt.mw.taskman.run_on_main(
             lambda: showWarning("Could not connect to Duolingo. Please check your internet connection.")
         )
         return result
 
-    language_string = vocabulary_response['language_string']
-    vocabs = vocabulary_response['vocab_overview']
+    current_language = lingo.get_user_info()['learning_language_string']
+    language_abbreviation = lingo.get_abbreviation_of(current_language)
+    vocabs = lingo.get_vocabulary(language_abbreviation)
+
+    for vocab in vocabs:
+        # A prior version of the Duolingo API exposed vocabulary ids, which we used to
+        # de-duplicate vocabs. This version does not, so we build our own new ids.
+        vocab['id'] = vocab['text'] + "-" + language_abbreviation
 
     did = mw.col.decks.get(DEFAULT_DECK_ID)['id']
     mw.col.decks.select(did)
@@ -94,8 +84,7 @@ def login_and_retrieve_vocab(username, password) -> VocabRetrieveResult:
     words_to_add = [vocab for vocab in vocabs if vocab['id'] not in gids_to_notes]
     result.success = True
     result.words_to_add = words_to_add
-    result.language_string = language_string
-    result.lingo = lingo
+    result.language_string = current_language
 
     return result
 
@@ -115,19 +104,19 @@ def add_vocab(retrieve_result: VocabRetrieveResult) -> AddVocabResult:
 
     total_word_count = len(retrieve_result.words_to_add)
     word_chunks = [retrieve_result.words_to_add[x:x + WORD_CHUNK_SIZE] for x in range(0, total_word_count, WORD_CHUNK_SIZE)]
-    lingo = retrieve_result.lingo
 
     aqt.mw.taskman.run_on_main(
         lambda: mw.progress.update(label=ADD_STATUS_TEMPLATE.format(0, total_word_count), value=0, max=total_word_count)
     )
 
+    def translations(vocab):
+        if vocab['translations']:
+            return '; '.join(vocab['translations'])
+        else:
+            return "Provide the translation for '{}' from {}.".format(vocab['text'], retrieve_result.language_string)
+
     words_processed = 0
     for word_chunk in word_chunks:
-        translations = {
-            vocab['word_string']: ["Provide the translation for '{}' from {}.".format(vocab['word_string'], retrieve_result.language_string)]
-            for vocab in word_chunk
-        }
-
         for vocab in word_chunk:
             n = mw.col.newNote()
 
@@ -135,26 +124,20 @@ def add_vocab(retrieve_result: VocabRetrieveResult) -> AddVocabResult:
             n._fmap = defaultdict(str, n._fmap)
 
             n['Gid'] = vocab['id']
-            n['Gender'] = vocab['gender'] if vocab['gender'] else ''
-            n['Source'] = '; '.join(translations[vocab['word_string']])
-            n['Target'] = vocab['word_string']
-            n['Pronunciation'] = vocab['normalized_string'].strip()
+            n['Gender'] = ''
+            n['Source'] = translations(vocab)
+            n['Target'] = vocab['text']
+            n['Pronunciation'] = ''
             n['Target Language'] = retrieve_result.language_string
             n.addTag(retrieve_result.language_string)
             n.addTag('duolingo_sync')
-
-            if vocab['pos']:
-                n.addTag(vocab['pos'])
-
-            if vocab['skill']:
-                n.addTag(vocab['skill'].replace(" ", "-"))
 
             num_cards = mw.col.addNote(n)
 
             if num_cards:
                 result.notes_added += 1
             else:
-                result.problem_vocabs.append(vocab['word_string'])
+                result.problem_vocabs.append(vocab['text'])
             words_processed += 1
 
             aqt.mw.taskman.run_on_main(
@@ -189,38 +172,36 @@ def sync_duolingo():
     aqt.mw.taskman.run_on_main(
         lambda: showWarning(
             """
-            <p><u>Warning, due to recent changes made by Duolingo in their code this plugin is now
-            <span style="color: red">unstable</span></u>. Specifically:
+            <p>Duolingo has been making breaking changes to their API every year or so. Duolingo does not officially
+            support any third party programs like this one, and I have found it more and more difficult to keep up with
+            their changes. Additionally, I don't use this plugin myself. I would therefore consider this add-on 
+            <u><span style="color: red">unsupported</span></u>. Specifically:
             <ul>
-            <li>You may have problems logging in. You might be able to fix these problems, or it might be
-            impossible for your account to log in at all.</li>
-            <li>In particular, it might not be possible for accounts that were created with Google or Facebook to use
-            this plugin.</li>
-            <li>You may encounter unhelpful error messages.</li>
+            <li>Any further changes by Duolingo which break this add-on might go unfixed.</li>
+            <li>Any release that I do make in response to a change by Duolingo might have disruptive changes. For example,
+            Duolingo might make some kind of information unavailable to this plugin which previously was available on its cards.</li>
+            <li>This add-on is only available for manual install from <a href="https://github.com/JASchilz/AnkiSyncDuolingo/releases">
+            https://github.com/JASchilz/AnkiSyncDuolingo/releases</a> and not the AnkiWeb site.</li>
+            <li>I'm inviting any programmer who uses this themselves and has an interest in maintaining it to take 
+            ownership at <a href="https://github.com/JASchilz/AnkiSyncDuolingo/issues/78">https://github.com/JASchilz/AnkiSyncDuolingo/issues/78</a>.</li>
             </p>
             
-            <p>If possible, we will work to resolve these issues. However, some issues may be impossible to resolve
-            or take weeks to resolve. You can follow this issue <a href="https://github.com/JASchilz/AnkiSyncDuolingo/issues/64">
-            here</a>. If you are encountering symptoms that are not described in that issue, then please chime in to
-            describe your problem.</p>
-            
-            <p>To see any other issues or open a new issue, see <a href="https://github.com/JASchilz/AnkiSyncDuolingo/issues">
+            <p>To see any issues or open a new issue, see <a href="https://github.com/JASchilz/AnkiSyncDuolingo/issues">
             the issue tracker</a>.</p>
             
-            <p>Click "OK" to log in to Duolingo. Note that you <u>must log in with your Duolingo username and password</u> and
-            <b>not log in with Google or Facebook</b>. You can view your username and set a password in <a href="https://www.duolingo.com/settings/account">
-            your Duolingo account profile</a>.
+            <p>Click "OK" to log in to Duolingo. This plugin will then pull words from <u>whatever language you have most
+            recently studied</u>.</p>
             """
         )
     )
     try:
-        username, password = duolingo_display_login_dialog(mw)
+        jwt = duolingo_display_login_dialog(mw)
     except TypeError:
         return
 
     op = QueryOp(
         parent=mw,
-        op=lambda col: login_and_retrieve_vocab(username, password),
+        op=lambda col: login_and_retrieve_vocab(jwt),
         success=on_retrieve_success,
     )
 
